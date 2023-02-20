70a7a92f4a34aa5db9305b0163b908d4f4c1dd0b
Bly Kim <jgkim@einsis.com>
2023-02-20 17:26:15 +0900

    0220 to rebase

--- src/ec/mod.rs
+++ src/ec/mod.rs
@@ -1,3 +1,4 @@
 mod test_ec;
+mod test_pk;
 
 pub use test_ec::{test_3, test_one, test_two};
--- src/ec/test_ec.rs
+++ src/ec/test_ec.rs
@@ -2,14 +2,14 @@ use std::{
     collections::HashMap,
     fs::File,
     io::{BufRead, BufReader, Write},
-    path::Path,
+    path::{Path, PathBuf},
 };
 
 use anyhow::{anyhow, bail, Result};
-use base64::{engine::general_purpose::STANDARD as base64_engine, Engine};
+// use base64::{engine::general_purpose::STANDARD as base64_engine, Engine};
 use chrono::{DateTime, NaiveDateTime, TimeZone, Utc};
-use data_encoding::BASE64;
-use pdu::{Ethernet, EthernetPdu};
+// use data_encoding::BASE64;
+// use pdu::{Ethernet, EthernetPdu};
 
 ///
 /// # Errors
@@ -133,7 +133,13 @@ pub fn test_3() {
         .timestamp_nanos();
     println!("{zerosec}, {onesec}, {twosec}");
 
-    println!("{:?}", Utc.timestamp_nanos(zerosec));
+    let serial_num = 1;
+    let mut pathbuf = PathBuf::from("value");
+    let time = Utc.timestamp_nanos(zerosec).format("%F").to_string();
+    let serial = format!("{:01$}", serial_num, 5);
+    let merged = format!("{time}-{serial}");
+    pathbuf.push(merged);
+    println!("{pathbuf:?}, {}", pathbuf.exists());
 
     let a: u32 = 1;
     let b: u32 = 50000;
@@ -147,45 +153,6 @@ pub fn test_3() {
 
     let a1 = format!("{a:0width$}", width = 5);
     println!("{a1}");
-
-    let dec = base64_engine.decode("//".as_bytes()).unwrap();
-    println!("{:?}", dec);
-    let aice = BASE64.decode("MjAyMi0xMi0yOFQwNzozOTozMi42OTE2MDlaIEVSUk9SIFRMUyBhbGVydCByZWNlaXZlZDogQWxlcnRNZXNzYWdlUGF5bG9hZCB7".as_bytes()).unwrap();
-    // println!("{:?}", aice);
-    let aiceto = std::str::from_utf8(&aice);
-    println!("{aiceto:?}");
-
-    let lalala: &[u8] = &[];
-
-    println!("{}", lalala.len());
-    // let testpack: &[u8] = &[
-    //     0x68, 0x5b, 0x35, 0xc0, 0x61, 0xb6, 0x00, 0x1d, 0x09, 0x94, 0x65, 0x38, 0x08, 0x00, 0x45,
-    //     0x00, 0x00, 0x3b, 0x2d, 0xfd, 0x00, 0x00, 0x40, 0x11, 0xbc, 0x43, 0x83, 0xb3, 0xc4, 0x2e,
-    //     0x83, 0xb3, 0xc4, 0xdc, 0x18, 0xdb, 0x18, 0xdb, 0x00, 0x27, 0xe0, 0x3e, 0x05, 0x1d, 0x07,
-    //     0x15, 0x08, 0x07, 0x65, 0x78, 0x61, 0x6d, 0x70, 0x6c, 0x65, 0x08, 0x07, 0x74, 0x65, 0x73,
-    //     0x74, 0x41, 0x70, 0x70, 0x08, 0x01, 0x31, 0x0a, 0x04, 0x1e, 0xcc, 0xe2, 0x51,
-    // ];
-    // println!("{:?}", testpack);
-
-    println!("\n\n");
-    // let testpacket = EthernetPdu::new(&testpack).unwrap();
-    // println!("{:?}", testpacket.inner());
-    let packet = EthernetPdu::new(&dec).unwrap();
-    println!("{:?}", packet.inner());
-    match packet.inner() {
-        Ok(Ethernet::Ipv4(_)) => println!("ipv4"),
-        Ok(Ethernet::Ipv6(_)) => println!("ipv6"),
-        Ok(other) => println!("unexpected protocol, {other:?}"),
-        Err(e) => println!("parser failure: {e:?}"),
-    }
-    // let src = packet.source_address();
-    // let dst = packet.destination_address();
-    // let proto = format!("protocol: 0x{:02x}", packet.protocol());
-
-    // println!("{src:?}, {dst:?}, {proto}");
-
-    // let asd = std::str::from_utf8(&dec[..10]);
-    // println!("{asd:?}");
 }
 
 fn reader(path: &str) -> Result<BufReader<File>> {
